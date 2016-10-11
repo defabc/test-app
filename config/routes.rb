8585da7751a37e692e12d0a639c1908b5385cb3a
defabc <defabc2231@gmail.com>
2016-10-12 06:55:53 +0900

    rails g model User,Userモデル実装,セッションコントローラ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'wellcome#index'
+  get 'auth/twitter/callback' => 'session#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
