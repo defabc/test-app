b747c52840b9206bfbe0dd93bdbfee4222889629
defabc <defabc2231@gmail.com>
2016-10-12 07:08:02 +0900

    ログアウト機能実装

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,4 +4,9 @@ class SessionController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path, notice: 'ログインに成功しました'
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
 end
