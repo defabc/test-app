ee5bb85f176cfdfc8cb2ca9f4e24c5fdc88ced2c
defabc <defabc2231@gmail.com>
2016-10-26 23:57:56 +0900

    イベント関連実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
     return unless logged_in?
     @current_user ||= User.find(session[:user_id])
   end
+
+  def authencate
+    redirect_to root_path, alert: 'ログインしてください' unless logged_in?
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+  before_action :authencate
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to root_path, notice: 'イベントが作成されました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :place, :start_time, :end_time, :content)
+  end
 end
